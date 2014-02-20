df1b17eace4f05ebc1fe8ed3490b5184e09c2032
Ramesh Nair <ram@hiddentao.com>
2014-02-20 18:04:32 +0800

    Remove demo app.

--- demo/src/controllers/main.js
+++
@@ -1,9 +0,0 @@
-var waigo = require('../../');
-
-var libMainController = waigo.load('lib:controllers/main');
-
-exports.index = function*(next) {
-  yield libMainController.index.call(this, next);
-};
-
-
