d97cb67b70241016b5e542e8fd93cd909f46a616
Ramesh Nair <ram@hiddentao.com>
2014-12-28 13:48:19 +0800

    Turn ES6 features on at runtime

--- src/cli/data/init/start-app.js
+++ src/cli/data/init/start-app.js
@@ -2,42 +2,17 @@
 "use strict";
 
 
+/**
+ * @fileOverview Executable script to bootstrap your app.
+ */
+
+
 // Turn on Harmony features
 var v8flags = require('v8-flags');
 v8flags.harmony(true);
 
 
-/**
- * @fileOverview The main entry point for your Waigo application.
- */
-
-
-var co = require('co'),
-  waigo = require('waigo');
-
-
-co(function*() {
-  /*
-  Initialise the Waigo framework.
-
-  If you need to override the application source folder and/or plugins to 
-  be loaded then this is the place to do it.
-
-   */
-  yield waigo.init();
-
-  /*
-  Start the application.
-
-  This loads in application configuration, runs all startup steps, sets up 
-  the middleware and kicks off the HTTP listener.
-   */
-  yield waigo.load('application').start();
-
-})(function(err) {
-  if (err) {
-    console.error(err.stack);
-  }
-});
+// bootstrap!
+require('waigo').load('bootstrap');
 
 
