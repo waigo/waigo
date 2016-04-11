0f90e6e609117f5c5eba2627d3ad783c517e5bd6
Ramesh Nair <ram@hiddentao.com>
2016-04-12 00:20:34 +0800

    added generic lib validator and sanitizer

---
+++ src/support/forms/sanitizers/lib.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const validator = require('validator');
+
+const waigo = global.waigo,
+  FieldValidationError = waigo.load('support/forms/field').FieldValidationError;
+
+
+/**
+ * Sanitizer which excutes a function from valiator lib
+ */
+module.exports = function(options) {
+  let { method, args } = options;
+
+  args = args || [];
+
+  if (!validator[method]) {
+    throw new FieldValidationError(`Invalid method: ${method}`);
+  }
+
+  return function*(field, value) {
+    return validator[method].apply(validator, [value].concat(args));
+  }
+};
+
---
+++ src/support/forms/validators/lib.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const validator = require('validator');
+
+const waigo = global.waigo,
+  FieldValidationError = waigo.load('support/forms/field').FieldValidationError;
+
+
+/**
+ * Validator which excutes a function from validator lib
+ *
+ * @throws Error If not an email address.
+ */
+module.exports = function(options) {
+  let { method, args } = options;
+
+  args = args || [];
+
+  if (!validator[method]) {
+    throw new FieldValidationError(`Invalid method: ${method}`);
+  }
+
+  return function*(context, field, value) {
+    if (!validator[method].apply(validator, [value].concat(args))) {
+      throw new FieldValidationError(`Validation failed: ${method}`);
+    }
+  }
+};
+
