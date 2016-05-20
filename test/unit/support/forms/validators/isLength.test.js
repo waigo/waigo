8b2e0f4dff8d382b72ebd1f5a5ae80b016843729
Ramesh Nair <ram@hiddentao.com>
2016-05-20 21:54:24 +0800

    isLength tests

--- test/unit/support/forms/validators/isLength.test.js
+++ test/unit/support/forms/validators/isLength.test.js
@@ -1,85 +1,64 @@
-var _ = require('lodash'),
+"use strict";
+
+const _ = require('lodash'),
   co = require('co'),
-  moment = require('moment'),
   path = require('path'),
   Q = require('bluebird');
 
-var _testUtils = require(path.join(process.cwd(), 'test', '_base'))(module),
-  test = _testUtils.test,
-  testUtils = _testUtils.utils,
-  assert = testUtils.assert,
-  expect = testUtils.expect,
-  should = testUtils.should,
-  waigo = testUtils.waigo;
+
+const test = require(path.join(process.cwd(), 'test', '_base'))(module);
+const waigo = global.waigo;
 
 
-var validator = null,
+let validator = null,
   validationResult = undefined;
 
 
 test['isLength'] = {
-  beforeEach: function(done) {
-    this.spy = test.mocker.spy(require('validator'), 'isLength');
+  beforeEach: function*() {
+    this.spy = this.mocker.spy(require('validator'), 'isLength');
 
-    waigo.__modules = {};
-    waigo.initAsync()
-      .then(function() {
-        validator = waigo.load('support/forms/validators/isLength');
-      })
-      .nodeify(done);
+    yield this.initApp();
+
+    validator = waigo.load('support/forms/validators/isLength');
   },
 
-  'calls through to validator module isLength()': function(done) {
-    var self = this;
+  'defaults': function*() {
     var fn = validator();
 
-    testUtils.spawn(fn, fn, null, 'test')
-      .finally(function() {
-        try {
-          self.spy.should.have.been.calledWithExactly('test', 0, 10000000);
-          done();
-        } catch (err) {
-          done(err);
-        }
-      });
+    co(fn(null, null, 0)).should.be.fulfilled;
+    co(fn(null, null, -1)).should.be.rejectedWith('Must be between 5 and 10000000 characters');
+
+    co(fn(null, null, 10000000)).should.be.fulfilled;
+    co(fn(null, null, 10000001)).should.be.rejectedWith('Must be between 5 and 10000000 characters');
   },
 
-  'too short': function(done) {
-    var self = this;
-
+  'too short': function*() {
     var fn = validator({
       min: 5
     });
 
-    testUtils.spawn(fn, fn, null, 'test')
-      .should.be.rejectedWith('Must be between 5 and 10000000 characters')
-      .and.notify(done);
+    co(fn(null, null, 'test'))
+      .should.be.rejectedWith('Must be between 5 and 10000000 characters');
   },
 
-  'too long': function(done) {
-    var self = this;
-
+  'too long': function*() {
     var fn = validator({
       max: 3
     });
 
-    testUtils.spawn(fn, fn, null, 'test')
-      .should.be.rejectedWith('Must be between 0 and 3 characters')
-      .and.notify(done);
+    co(fn(null, null, 'test'))
+      .should.be.rejectedWith('Must be between 0 and 3 characters');
   },
 
 
-  'pass': function(done) {
-    var self = this;
-
+  'pass': function*() {
     var fn = validator({
       min: 4, 
       max: 5,
     });
 
-    testUtils.spawn(fn, fn, null, 'test')
-      .should.be.fulfilled
-      .and.notify(done);
+    yield fn(null, null, 'test');
   }
 
 };
