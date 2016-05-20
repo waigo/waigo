019fa74411f13717ff8d1c32e63a753153ee5cb5
Ramesh Nair <ram@hiddentao.com>
2016-05-20 21:50:02 +0800

    isEmailAddress test

--- test/unit/support/forms/validators/isEmailAddress.test.js
+++ test/unit/support/forms/validators/isEmailAddress.test.js
@@ -1,71 +1,54 @@
-var _ = require('lodash'),
+"use strict";
+
+const _ = require('lodash'),
   co = require('co'),
-  moment = require('moment'),
   path = require('path'),
   Q = require('bluebird');
 
-var _testUtils = require(path.join(process.cwd(), 'test', '_base'))(module),
-  test = _testUtils.test,
-  testUtils = _testUtils.utils,
-  assert = testUtils.assert,
-  expect = testUtils.expect,
-  should = testUtils.should,
-  waigo = testUtils.waigo;
+
+const test = require(path.join(process.cwd(), 'test', '_base'))(module);
+const waigo = global.waigo;
 
 
-var validator = null,
+let validator = null,
   validationResult = undefined;
 
 
 test['isEmailAddress'] = {
-  beforeEach: function(done) {
-    this.spy = test.mocker.stub(require('validator'), 'isEmail', function() { 
+  beforeEach: function*() {
+    this.spy = this.mocker.stub(require('validator'), 'isEmail', function() { 
       return validationResult; 
     });
 
-    waigo.__modules = {};
-    waigo.initAsync()
-      .then(function() {
-        validator = waigo.load('support/forms/validators/isEmailAddress');
-      })
-      .nodeify(done);
+    yield this.initApp();
+
+    validator = waigo.load('support/forms/validators/isEmailAddress');
   },
 
-  'calls through to validator module isEmail()': function(done) {
-    var self = this;
+  'calls through to validator module isEmail()': function*() {
     var fn = validator();
 
-    testUtils.spawn(fn, fn, null, 'test')
-      .finally(function() {
-        try {
-          self.spy.should.have.been.calledWithExactly('test');
-          done();
-        } catch (err) {
-          done(err);
-        }
-      });
+    try {
+      yield fn(null, null, 'test');
+    } catch (err) {
+      /* nothing */
+    } finally {
+      this.spy.should.have.been.calledWithExactly('test');
+    }
   },
 
-  'fail': function(done) {
-    var self = this;
-
+  'fail': function*() {
     var fn = validator();
     validationResult = false;
 
-    testUtils.spawn(fn, fn, null, 'test')
-      .should.be.rejectedWith('Must be an email address')
-      .and.notify(done);
+    co(fn(null, null, 'test')).should.be.rejectedWith('Must be an email address');
   },
 
-  'pass': function(done) {
-    var self = this;
-
+  'pass': function*() {
     var fn = validator();
     validationResult = true;
 
-    testUtils.spawn(fn, fn, null, 'test')
-      .should.be.fulfilled
-      .and.notify(done);
+    yield fn(null, null, 'test');
   }
 
 };
