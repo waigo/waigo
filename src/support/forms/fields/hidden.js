156716a4d7b55740d1ce9ce8d74ea48cafa8e81b
Ramesh Nair <ram@hiddentao.com>
2016-05-22 20:52:47 +0800

    added csrf field test

--- src/support/forms/fields/hidden.js
+++ src/support/forms/fields/hidden.js
@@ -8,4 +8,17 @@ const waigo = global.waigo,
 class HiddenField extends TextField {}
 
 
+
+/**
+ * @override
+ */
+HiddenField.prototype[viewObjects.METHOD_NAME] = function*(ctx) {
+  let ret = yield TextField.prototype[viewObjects.METHOD_NAME].call(this, ctx);
+
+  ret.type = 'hidden';
+
+  return ret;
+};
+
+
 module.exports = HiddenField;
