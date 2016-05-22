7ff7ad800c3a3d023c27d520505190d0b3f2fabc
Ramesh Nair <ram@hiddentao.com>
2016-05-22 20:58:35 +0800

    upgrade test utils

--- test/_base.js
+++ test/_base.js
@@ -21,58 +21,6 @@ module.exports = function(_module) {
         
         throw new Error('Expected error not thrown: ' + err);
       },
-      clearDb: function*(modelName) {
-        let models;
-
-        if (modelName) {
-          models = [modelName];
-        } else {
-          models = ['Acl', 'Activity', 'Cron', 'User'];
-        }
-
-        for (let model of models) {
-          yield this.app.models[model].rawQry().delete().run();
-        }
-      },
-      initApp: function*() {
-        waigo.reset();
-
-        yield waigo.init({
-          appFolder: this.appFolder,
-        });
-
-        this.Application = waigo.load('application');
-        this.app = this.Application.app;
-      },
-      startApp: function*(config) {
-        config = _.extend({
-          logging: {
-            category: "test",
-            minLevel: 'DEBUG',
-            appenders: [],
-          },      
-          db: {
-            main: {
-              type: 'rethinkdb',
-              serverConfig: {
-                db: 'waigo_test',
-                servers: [
-                  {
-                    host: '127.0.0.1',
-                    port: 28015,
-                  },
-                ],
-              },
-            },
-          },
-        }, config);
-
-        yield this.Application.start({
-          postConfig: (cfg) => {
-            _.extend(cfg, config);
-          },
-        });
-      },
     },
   });
 
