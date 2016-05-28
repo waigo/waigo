8d5232cf6e3d8495c818a863dd4ea070703dc000
Ramesh Nair <ram@hiddentao.com>
2016-05-28 18:51:42 +0800

    lodash mixin test

--- src/support/lodashMixins.js
+++ src/support/lodashMixins.js
@@ -11,10 +11,8 @@ module.exports = function(_) {
     pluralize: require('pluralize'),
     uuid: require('node-uuid'),
     /**
-     * Format user name for display within email.
+     * Format user name for display.
      *
-     * TODO: This method is confusing - clean it up!
-     * 
      * @param  {String} type How to format.
      * @param  {User|String} obj  The user object or username string.
      * @return {*}
@@ -22,9 +20,9 @@ module.exports = function(_) {
     emailFormat: function(type, obj) {
      switch (type) {
        case 'greet':
-         var name = _.get(obj, 'profile.displayName') || _.get(obj, 'username', '');
+         var name = _.get(obj, 'profile.displayName') || _.get(obj, 'username', '') || obj;
 
-         return name.length ? name : 'Hey';
+         return _.get(name, 'length') ? name : 'Hey';
          break;
      }
 
