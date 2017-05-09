4936acca6cb047494f991b7386f73effe6357f06
Ramesh Nair <ram@hiddentao.com>
2017-05-09 09:36:31 +0100

    cron shutdown test

---
+++ test/unit/cron/support/shutdown.test.js
@@ -0,0 +1,32 @@
+const path = require('path')
+
+const test = require(path.join(process.cwd(), 'test', '_base'))(module)
+
+
+test['cron shutdown'] = {
+  beforeEach: function *() {
+    yield this.initApp()
+
+    yield this.startApp({
+      startupSteps: []
+    })
+
+    this.method = this.waigo.load('cron/support/shutdown')
+  },
+  afterEach: function *() {
+    yield this.shutdownApp()
+  },
+  'destroys cron': function *() {
+    let called = false
+
+    this.App.cron = {
+      destroy: function *() {
+        called = true
+      }
+    }
+
+    yield this.method(this.App)
+
+    called.must.be.true()
+  },
+}
