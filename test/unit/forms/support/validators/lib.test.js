6b76348d2778db0069c638459d8ae0115db3345f
Ramesh Nair <ram@hiddentao.com>
2017-05-20 23:16:30 +0100

    lib and notEmpty tests

--- test/unit/forms/support/validators/lib.test.js
+++ test/unit/forms/support/validators/lib.test.js
@@ -1,42 +1,30 @@
-
-
-const _ = require('lodash'),
-  co = require('co'),
-  path = require('path'),
-  Q = require('bluebird')
-
+const path = require('path')
 
 const test = require(path.join(process.cwd(), 'test', '_base'))(module)
-const waigo = global.waigo
-
-
-const validator = null,
-  validationResult = undefined
-
 
 test['lib'] = {
   beforeEach: function *() {
     yield this.initApp()
 
-    validator = this.waigo.load('forms/support/validators/lib')
+    this.validator = this.waigo.load('forms/support/validators/lib')
   },
 
   'invalid method': function *() {
     expect(() => {
-      validator({
+      this.validator({
         method: 'blahblah'
       })
     }).to.throw('Invalid method: blahblah')
   },
 
   'calls method': function *() {
-    const fn = validator({
+    const fn = this.validator({
       method: 'isLength',
       args: [3, 4],
     })
 
-    yield this.must.Throw(fn(null, null, 'ab'), 'Validation failed: isLength')
-    yield this.must.Throw(fn(null, null, 'abcde'), 'Validation failed: isLength')
+    yield this.awaitAsync(fn(null, null, 'ab')).must.reject.with.error('Validation failed: isLength')
+    yield this.awaitAsync(fn(null, null, 'abcde')).must.reject.with.error('Validation failed: isLength')
     yield fn(null, null, 'abcd')
     yield fn(null, null, 'abc')
   },
--- test/unit/forms/support/validators/notEmpty.test.js
+++ test/unit/forms/support/validators/notEmpty.test.js
@@ -1,57 +1,46 @@
-
-
-const _ = require('lodash'),
-  co = require('co'),
-  path = require('path'),
-  Q = require('bluebird')
-
+const path = require('path')
 
 const test = require(path.join(process.cwd(), 'test', '_base'))(module)
-const waigo = global.waigo
-
-
-const validator = null
-
 
 test['notEmpty'] = {
   beforeEach: function *() {
     yield this.initApp()
 
-    validator = this.waigo.load('forms/support/validators/notEmpty')
+    this.validator = this.waigo.load('forms/support/validators/notEmpty')
   },
 
   'null': function *() {
-    const fn = validator()
+    const fn = this.validator()
 
-    yield this.must.Throw(fn(null, null, null), 'Must not be empty')
+    yield this.awaitAsync(fn(null, null, null)).must.reject.with.error('Must not be empty')
   },
   'undefined': function *() {
-    const fn = validator()
+    const fn = this.validator()
 
-    yield this.must.Throw(fn(null, null, undefined), 'Must not be empty')
+    yield this.awaitAsync(fn(null, null, undefined)).must.reject.with.error('Must not be empty')
   },
   'empty string': function *() {
-    const fn = validator()
+    const fn = this.validator()
 
-    yield this.must.Throw(fn(null, null, ''), 'Must not be empty')
+    yield this.awaitAsync(fn(null, null, '')).must.reject.with.error('Must not be empty')
   },
   'non-empty string': function *() {
-    const fn = validator()
+    const fn = this.validator()
 
     yield fn(null, null, 'a')
   },
   'number': function *() {
-    const fn = validator()
+    const fn = this.validator()
 
     yield fn(null, null, 0)
   },
   'boolean: true': function *() {
-    const fn = validator()
+    const fn = this.validator()
 
     yield fn(null, null, true)
   },
   'boolean: false': function *() {
-    const fn = validator()
+    const fn = this.validator()
 
     yield fn(null, null, false)
   }
